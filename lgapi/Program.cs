831e13fca34a3d015c9d59af6c2671678962beed
Theerakarn-Kangonta <theerakarn.kangonta@gmail.com>
2025-05-11 01:24:17 +0700

    add docker file

--- lgapi/Program.cs
+++ lgapi/Program.cs
@@ -5,9 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var corsPolicyName = "AllowAngularApp";
 
 builder.Services.AddCors(options =>
